7f35349b70368b5da6d757ad265d87aeb64c43f2
hiccup444 <sunnylilttledragon@gmail.com>
2026-06-21 08:34:35 -0400

    Update InputManager
    Fix application losing focus for webgl compatability

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -7,42 +7,26 @@ public class InputManager : MonoBehaviour
 
     private void Awake()
     {
-        // Create the input system instance
         input = new PlayerInputActions();
     }
 
     private void OnEnable()
     {
-        // Turn input on when this object is active
         input.Player.Enable();
     }
 
     private void OnDisable()
     {
-        // Turn input off when this object is disabled
         input.Player.Disable();
     }
 
-    /// <summary>
-    /// Returns the current movement direction from the player.
-    /// Value is a Vector2 where X = left/right, Y = up/down (if used).
-    /// </summary>
-    public Vector2 GetMove()
+    private void OnApplicationFocus(bool hasFocus)
     {
-        return input.Player.Move.ReadValue<Vector2>();
+        if (!hasFocus) input.Player.Disable();
+        else input.Player.Enable();
     }
 
-    /// <summary>
-    /// Returns true only on the frame the jump button is pressed.
-    /// Used for actions that should happen once per press (like jumping).
-    /// </summary>
-    public bool GetJumpPressed()
-    {
-        return input.Player.Jump.triggered;
-    }
-
-    public bool GetSpinPressed()
-    {
-        return input.Player.Spin.triggered;
-    }
+    public Vector2 GetMove() => input.Player.Move.ReadValue<Vector2>();
+    public bool GetJumpPressed() => input.Player.Jump.triggered;
+    public bool GetSpinPressed() => input.Player.Spin.triggered;
 }
