435a449c325a7f01816e6274bae7e98c6ffcd57b
silh0utte <umar.ghazari@gmail.com>
2026-06-24 16:22:06 +0800

    able to close dialogue now

--- Assets/Developers/Skully/DialogueText.cs
+++ Assets/Developers/Skully/DialogueText.cs
@@ -57,5 +57,9 @@ public class DialogueText : MonoBehaviour
             dialogueText.text = string.Empty;
             StartCoroutine(TypeLine());
         }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
